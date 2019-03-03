f25118c2688f7704c68f2c32f71831fa54c8551d
MikelDementyev <mike.new.steam@gmail.com>
2019-03-04 00:40:11 +0300

    AStar is fixed

--- Dementyev_M_E/lab2/secondLab_A_star.cpp
+++ Dementyev_M_E/lab2/secondLab_A_star.cpp
@@ -1,15 +1,13 @@
 //
 //  main.cpp
-//  SecondLab2
+//  AStar
 //
-//  Created by Михаил Дементьев on 24/02/2019.
+//  Created by Михаил Дементьев on 25/02/2019.
 //  Copyright © 2019 Михаил Дементьев. All rights reserved.
 //
 
 #include <iostream>
 #include <vector>
-#include <cstring>
-#include <string>
 #include <algorithm>
 
 using namespace std;
@@ -25,15 +23,8 @@ struct ways {
     double len;
 };
 
-struct fork {
-    char symbol;
-    unsigned long ind;
-    double len;
-};
-
-struct answer{
-    vector<char> ans;
-    vector<int> lenMass;
+struct newWay {
+    vector<char> way;
     double len;
 };
 
@@ -44,30 +35,22 @@ bool customer_sorter(ways const& lhs, ways const& rhs) { //Сортировка 
 
 int main() {
     
+    vector<struct newWay> answerWays;
+    int countOfWays = 0;
+    answerWays.resize(100);
     vector<ways> ways;
-    struct answer answer;           //Массив временных ответов
-    struct answer realAnswer;       //Массив минимального ответа
-    answer.len = 0;
-    realAnswer.len = 1000;
-    vector<fork> massOfForks;
-    
-    //bool firstFlag = true;
+    vector<char> bestWay;
+    bool endFlag = false;
     
     cin >> start_finish.start >> start_finish.finish;
     
-    string data;
-    //char* charData = new char[100];
-    
-    //struct ways way;
-    
     int flagCount = 0;
-    
     while (1){
         struct ways way;
         
         if(way.finPos == start_finish.finish)
             flagCount++;
-        if(flagCount>0) break;
+        if(flagCount>1) break;
         
         if(!(cin >> way.stPos)){
             break;
@@ -82,92 +65,89 @@ int main() {
     }
     
     sort(ways.begin(), ways.end(), &customer_sorter);
-    /*
-     for (int i = 0; i < ways.size(); i++){
-     cout << ways[i].finPos;
-     }
-     */
-    /*
-     while (getline(cin, data)){
-     if(firstFlag){
-     
-     start_finish.start = data[0];
-     start_finish.finish = data[2];
-     
-     } else {
-     struct ways way;
-     
-     charData = strdup(data.c_str());
-     
-     char* pch;
-     pch = strtok(charData, " ");
-     way.stPos = pch[0];
-     
-     pch = strtok(NULL, " ");
-     way.finPos = pch[0];
-     
-     pch = strtok(NULL, " ");
-     way.len = atof(pch);
-     
-     ways.push_back(way);
-     }
-     
-     firstFlag = false;
-     }
-     */
     
-    answer.ans.push_back(start_finish.start);
-    answer.lenMass.push_back(0);
+    answerWays[countOfWays].way.push_back(start_finish.start);
+    answerWays[countOfWays].len = 0;
+    countOfWays++;
     
-    while(1){       //Бесконечный цикл с условие выхода внутри
-        struct ways ansWay;
-        bool firstFlag = true;
-        for (int i = 0; i < ways.size(); i++){  //Отличается от прошлого тем, что в массиве ответов помимо самих вершин мы сохраняем длину пути
-            if(ways[i].stPos == answer.ans[answer.ans.size()-1]){
-                if (firstFlag) {
-                    ansWay = ways[i];
-                    firstFlag = false;
+    while(!endFlag){
+        vector<struct ways>tempWay;
+        for(int i = 0; i < countOfWays; i++){
+            for(int j = 0; j < ways.size(); j++){
+                if(answerWays[i].way[answerWays[i].way.size()-1] == ways[j].stPos){
+                    tempWay.push_back({ways[j].stPos, ways[j].finPos, answerWays[i].len+ways[j].len});
+                    //ways.erase(ways.begin()+j);
+                }
+            }
+        }
+        
+        struct ways bestTempWay;
+        bestTempWay.len = 500;
+        for(int i = 0; i < tempWay.size(); i++){
+            if(tempWay[i].len < bestTempWay.len){
+                bestTempWay = tempWay[i];
+            } else if (tempWay[i].len == bestTempWay.len){
+                if(tempWay[i].finPos > bestTempWay.finPos){
+                    bestTempWay = tempWay[i];
+                }
+                if(tempWay[i].stPos > bestTempWay.stPos){
+                    bestTempWay = tempWay[i];
+                }
+            }
+        }
+        
+        for(int i = 0; i < ways.size(); i++){
+            if(ways[i].stPos == bestTempWay.stPos && ways[i].finPos == bestTempWay.finPos){
+                ways.erase(ways.begin()+i);
+            }
+        }
+        
+        for(int i = 0; i < countOfWays; i++){
+            if(answerWays[i].way[answerWays[i].way.size()-1] == bestTempWay.stPos){
+                if(tempWay.size() > 1){
+                    answerWays[countOfWays] = answerWays[i];
+                    countOfWays++;
+                }
+                answerWays[i].way.push_back(bestTempWay.finPos);
+                answerWays[i].len = bestTempWay.len;
+                if(bestTempWay.finPos == start_finish.finish){
+                    for(int i = 0; i < countOfWays; i++){
+                        if(answerWays[i].way[answerWays[i].way.size()-1] == bestTempWay.stPos){
+                            answerWays[i].way.push_back(bestTempWay.finPos);
+                            answerWays[i].len = bestTempWay.len;
+                        }
+                    }
+                }
+                break;
+            }
+        }
+        
+        tempWay.erase(tempWay.begin(), tempWay.end());
+        tempWay.clear();
+        
+        vector<char>tempBest;
+        
+        for(int i = 0; i < countOfWays; i++){
+            if(answerWays[i].way[answerWays[i].way.size()-1] == start_finish.finish){
+                if(!endFlag){
+                    bestWay = answerWays[i].way;
+                    endFlag = true;
                 } else {
-                    massOfForks.push_back({ways[i].finPos, answer.ans.size(), ways[i].len});
-                    if((int)(ansWay.finPos) - (int)(start_finish.finish) < (int)(ways[i].finPos) - (int)(start_finish.finish)){
-                        massOfForks[massOfForks.size()-1] = {ansWay.finPos, answer.ans.size(), ansWay.len};
-                        ansWay = ways[i];
+                    for(int j = 0; j < min(answerWays.size(), bestWay.size()); j++){
+                        if(answerWays[i].way[j] > bestWay[j]){
+                            bestWay = answerWays[i].way;
+                            break;
+                        }
                     }
                 }
             }
         }
-        if(firstFlag || answer.ans[answer.ans.size()-1] == start_finish.finish){ //Если не было найдено пути или мы в конечной точке
-            if(massOfForks.size() == 0) //Если массив развилок пуст, заканчиваем работу алгоритма
-                break;
-            if(answer.ans[answer.ans.size()-1] == start_finish.finish){ //Если мы в конечной точке
-                if(answer.len < realAnswer.len){    //Если временный ответ меньше минимального, присваиваем минимальному временный
-                    realAnswer = answer;
-                }
-            }
-            for (unsigned long i = massOfForks[massOfForks.size()-1].ind; i < answer.lenMass.size(); i++){  //Во время очистки массива ответов вполоть до первой развилки, уменьшаем длинну пути.
-                answer.len -= answer.lenMass[i];
-            }   //Продолжаем путь с первой развилки, обрезая справа массив ответов и удаляю последнюю развилку из массива непосещенных вершин
-            answer.ans.erase(answer.ans.begin() + massOfForks[massOfForks.size()-1].ind, answer.ans.begin() + answer.ans.size());
-            answer.lenMass.erase(answer.lenMass.begin() + massOfForks[massOfForks.size()-1].ind, answer.lenMass.begin() + answer.lenMass.size());
-            answer.ans.push_back(massOfForks[massOfForks.size()-1].symbol);
-            answer.lenMass.push_back(massOfForks[massOfForks.size()-1].len);
-            answer.len += massOfForks[massOfForks.size()-1].len;
-            massOfForks.erase(massOfForks.begin() + (massOfForks.size()-1));
-            
-        } else { //Если был найден путь, заносим его в массив ответов, увеличив при этом значение длинны
-            answer.ans.push_back(ansWay.finPos);
-            answer.lenMass.push_back(ansWay.len);
-            answer.len += ansWay.len;
-            firstFlag = true;
-        }
     }
     
-    for (int i = 0; i < realAnswer.ans.size(); i++){ //Выводим на экран массив ответов.
-        cout << realAnswer.ans[i];
+    for (int i = 0; i < bestWay.size(); i++){
+        cout << bestWay[i];
     }
-    cout << endl;
+    
     return 0;
 }
-
-
 
